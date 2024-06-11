885fd0bc436beb426950e2f8e0fe6aeb27bd6201
İbrahim Okumuş <ibrahimokumus_09@hotmail.com>
2024-06-11 23:37:38 +0300

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import MainHeader from "./components/MainHeader";
 import Post from "./components/Post";
 import PostsList from "./components/PostsList";
+import { useState } from "react";
 function App(){
+  const [modelIsVisible,setModelIsVisible ] = useState(true);
+
+  function showModalHandler()
+  {
+    setModelIsVisible(true)
+  }
+  function hideModalHandler(){
+    setModelIsVisible(false)
+  }
   return(
-    <main>
-      <PostsList/>
-    </main>
+    <>
+      <MainHeader onCreatePost={showModalHandler}/>
+      <main>
+      <PostsList
+        isPosting={modelIsVisible}
+        onStopPosting = {hideModalHandler}
+      />
+      </main> 
+    </>
+
+    
     );
 }
 export default App
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,25 +1,61 @@
 import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Modal from './Modal'
 
-function PostsList(){
-    const [enteredBody,setEnteredBody] =useState('');
-    const [enteredAuthor,setEnteredAuthor] =useState('');
-    
-    function changeBodyHandler(event){
-        setEnteredBody(event.target.value);
-    }
-    function changeAuthorHandler(event){
-        setEnteredAuthor(event.target.value);
-    }
+function PostsList({isPosting,onStopPosting}){
+   // fetch('http://localhost:8080/posts').then(response =>response.json()).then(data =>{setPosts(data.posts)});
+    const [posts,setPosts] = useState([]);
+    const [isFetching,setIsFetching]= useState(false);
+   
+    useEffect(()=>{
+        async function fetchPosts(){
+            setIsFetching(true);
+            const response = await fetch('http://localhost:8080/posts')
+            const resData =  await response.json();
+            setPosts(resData.posts);
+            setIsFetching(false);
+        }
+        fetchPosts();
+        }, []);
+    function addPostHandler(postData){
+        fetch('http://localhost:8080/posts',{
+            method:'POST',
+            body:JSON.stringify(postData), 
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
+        setPosts((existingPosts)=>[postData,...existingPosts]);
+   }
     return(
         <>
-            <NewPost onChangeBody={changeBodyHandler} onChangeAuthor={changeAuthorHandler}/>
-            <ul className={classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody}/>
-                <Post author = "Menual" body="Hello World!"/>  
-            </ul>
+            {isPosting && (<Modal onClose={onStopPosting}>
+                <NewPost
+                    onCancel={onStopPosting}
+                    onAddPost={addPostHandler}
+                />
+            </Modal>)}
+            {!isFetching && posts.length>0 && (
+                <ul className={classes.posts}>
+                    {posts.map((post)=>(
+                        <Post key={post.body} author={post.author} body={post.body}/>   
+                    ))}  
+                </ul>
+            )}
+            {!isFetching && posts.length === 0 && (
+                <div style={{textAlign: 'center', color:'white'}}>
+                    <h2>There is no post yet</h2>
+                    <p>Start adding some!</p>
+                </div>
+            )}
+            {isFetching && (
+                <div style={{textAlign:'center',color:'white'}}>
+                    <p>Loading posts... </p>   
+                </div>
+            )}
+            
        </>
     );
 }
